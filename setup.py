4312873b25c34c18771c091357ccc4b2fd2ef8b6
Omri Mendels <omri374@users.noreply.github.com>
2020-04-19 00:08:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="openvino-text-spotting",
     use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     packages=find_packages(),
     install_requires=open("requirements.txt").readlines()
 )
